befec16f0445dff811dec8201d3d71e2d2f1b860
MitroxYT <168591279+MitroxYT@users.noreply.github.com>
2026-02-12 00:03:30 +0300

    Update AimF.kt

--- src/main/java/me/onixdev/check/impl/combat/aim/AimF.kt
+++ src/main/java/me/onixdev/check/impl/combat/aim/AimF.kt
@@ -13,17 +13,31 @@ class AimF(player:OnixUser) : Check(player,CheckBuilder.create().setCheckName("A
     override fun onEvent(event: BaseEvent?) {
         if (event is PlayerRotationEvent && !event.isPost && player.lastHitTime < 4) {
             if (player.lastTeleportTime < 5 && player.movementContainer.deltaXZ < 0.09) return
-            var yawDiff: Float = abs(player.rotationContainer.lastYaw - player.rotationContainer.yaw)
-            if (yawDiff > 180.0f) {
-                yawDiff = 360.0f - yawDiff
-            }
-            if (yawDiff > minFovFactor) {
+            val rotationData = player.rotationContainer
+            val dx = rotationData.deltaYaw;
+            val dy = rotationData.deltaPitch;
+            val sx = rotationData.smoothnessYaw;
+            val sy = rotationData.smoothnessPitch;
+
+            if (dx > minFovFactor && sx < -5.0F && player.movementContainer.deltaXZ > 0.05) {
+                val info = String.format("dx=%.5f, dy=%.5f, sx=%.5f, sy=%.5f ", dx, dy, sx, sy)
                 if (lastTickFlagget) {
-                    fail(String.format("%.5f", yawDiff))
+                    fail(info)
+                    lastTickFlagget = false
                 }
                 lastTickFlagget = true
+//            var yawDiff: Float = abs(player.rotationContainer.lastYaw - player.rotationContainer.yaw)
+//            if (yawDiff > 180.0f) {
+//                yawDiff = 360.0f - yawDiff
+//            }
+//            if (yawDiff > minFovFactor) {
+//                if (lastTickFlagget) {
+//                    fail(String.format("%.5f", yawDiff))
+//                }
+//                lastTickFlagget = true
+//            }
+                //  else lastTickFlagget = false
             }
-            else lastTickFlagget = false
         }
     }
 
